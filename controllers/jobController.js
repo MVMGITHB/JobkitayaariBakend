18f86c78b321705b6fb07c052c072f855fe37ea6
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-06-19 13:30:48 +0530

    Active Job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -5,9 +5,9 @@ import SubCategory from '../models/SubCategory.js';
 export const createJob = async (req, res) => {
   try {
     const job = new Job({
-          ...req.body,
-          slug: slugify(req.body.slug).toLowerCase(),
-        });
+      ...req.body,
+      slug: slugify(req.body.slug).toLowerCase(),
+    });
 
 
     await job.save();
@@ -21,7 +21,7 @@ export const createJob = async (req, res) => {
 export const getAllJobs = async (req, res) => {
   try {
     const jobs = await Job.find().populate('category subCategory');
-     res.status(200).json(jobs);
+    res.status(200).json(jobs);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -35,8 +35,10 @@ export const GetJobByCategory = async (req, res) => {
       return res.status(404).json({ message: 'Subcategory not found' });
     }
 
-    const job = await Job.find({ subCategory: subcatagory._id })
-      .populate('category subCategory')
+    const job = await Job.find({
+      subCategory: subcatagory._id,
+      status: 'Active'
+    }).populate('category subCategory')
       .sort({ status: 1 }); // 'Active' comes before 'Inactive' if using alphabetical sort
 
     res.status(200).json(job);
@@ -63,25 +65,25 @@ export const getJobById = async (req, res) => {
 
 // Get a single job by slug
 export const getJobBySlug = async (req, res) => {
-    try {
-      const job = await Job.findOne({ slug: req.params.slug }).populate('category subCategory');
-      if (!job) {
-        return res.status(404).json({ message: 'Job not found' });
-      }
-      res.status(200).json(job);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+  try {
+    const job = await Job.findOne({ slug: req.params.slug }).populate('category subCategory');
+    if (!job) {
+      return res.status(404).json({ message: 'Job not found' });
     }
-  };
-  
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 
 // Update a job
 export const updateJob = async (req, res) => {
   try {
     const job = await Job.findByIdAndUpdate(req.params.id, {
-          ...req.body,
-          slug: slugify(req.body.slug).toLowerCase(),
-        }, { new: true });
+      ...req.body,
+      slug: slugify(req.body.slug).toLowerCase(),
+    }, { new: true });
     if (!job) {
       return res.status(404).json({ message: 'Job not found' });
     }
@@ -97,17 +99,17 @@ export const updateStatus = async (req, res) => {
   try {
 
     let jobs = await Job.findById(req.params.id)
-   
+
 
     if (!jobs) return res.status(404).json({ error: "Job not found" });
 
-    if(jobs.status ==='Inactive')  {
-      jobs.status ='Active'
-    }else{
-      jobs.status ='Inactive'
+    if (jobs.status === 'Inactive') {
+      jobs.status = 'Active'
+    } else {
+      jobs.status = 'Inactive'
     }
 
-   const Jobs =  await  jobs.save()
+    const Jobs = await jobs.save()
 
     res.json(Jobs);
   } catch (err) {
